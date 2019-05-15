c0cc80849d9c7a81829a141cc8b28d23348f39b5
sanket17-amazatic <sanket.t@amazatic.com>
2019-05-15 14:43:02 +0530

    requirement changes

--- src/core/settings/production.py
+++ src/core/settings/production.py
@@ -6,6 +6,8 @@ import os
 import json
 import dj_database_url
 
+SECRET_KEY = 'mq=1@_3wwgry$rs8oi&45pxr0_da=4i311d52j(l-a0%_p9(jt'
+
 DEBUG = True if (os.environ.get("DEBUG", "false").lower() == "true") else False
 
 DEFAULT_CONNECTION = dj_database_url.parse(os.environ.get("DATABASE_URL"))
